3c5eae6311de1446e5f0fddb1bd932e532986341
MrCurryMans <53878734+MrCurryMans@users.noreply.github.com>
2025-04-25 15:49:47 +0100

    transparent bg logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           <div className="flex-shrink-0 flex items-center">
             <Link to="/" className="flex items-center">
               <img 
-                src="/lovable-uploads/3e7c53b2-1a93-4204-b584-6168c13e1edb.png" 
+                src="MrCurryMans/wearemore/public/lovable-uploads/Logo_Higher_Quality__1_-removebg-preview.png" 
                 alt="MORE Logo" 
                 className="h-12 w-auto"
               />
